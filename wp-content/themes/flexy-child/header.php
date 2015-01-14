3c82fa87b7a897ad7e4fd2ec8b1cd3efa1559fac
Vanessa Garcia <me@vanessa-lyn.com>
2015-01-14 15:32:18 -0700

    deleted unused themes. began global site styling. beginning sidebar Featured Artists menu and logo.

---
+++ wp-content/themes/flexy-child/header.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The Header template for our theme
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package Flexy
+ 
+ * @since Flexy 1.0
+ */
+?><!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) & !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<meta name="twitter:widgets:csp" content="on">
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<link href='http://fonts.googleapis.com/css?family=Droid+Sans:400,700|Josefin+Sans:300' rel='stylesheet' type='text/css'>
+<?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
+<!--[if lt IE 9]>
+<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+<![endif]-->
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed site">
+
+	<div id="main" class="wrapper">
